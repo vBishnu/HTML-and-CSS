fab09c9c6ca04e7b4e2a2a17a21f35cd513357ae
vBishnu <bishnu.projectile@gmail.com>
2025-11-25 14:16:05 +0545

    nodemon

---
+++ Week 4/Node Project -01/app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+ const app=express
+
+ app.get=("./",(req,res)=>{
+    res.send()
+ })
+
+ app.get=("./home",(req,res)=>{
+    res.send()
+ })
+
+  app.get=("./about us",(req,res)=>{
+    res.send()
+ })
+
+  app.get=("./contact",(req,res)=>{
+    res.send()
+ })
+
